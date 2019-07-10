ef92255522b72004544010e8c68585f084a9e33e
Cristian <cristian.medina94@gmail.com>
2019-07-10 00:46:39 -0300

    correccion en una variable mal escrita

--- src/main/java/prueba/servicios/Repositorio.java
+++ src/main/java/prueba/servicios/Repositorio.java
@@ -5,5 +5,5 @@ import java.util.List;
 public interface Repositorio {
 	public String invertirTexto(String txt);
 	
-	public List<String> permutar(String txts);
+	public List<String> permutar(String txt);
 }
